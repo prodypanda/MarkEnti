246591f32d7db4d7057125ac1ad487a2ebafab61
Prodypanda <admin@prodypanda.com>
2024-02-28 07:20:07 +0100

    min update

--- src/routes/inventory.routes.js
+++ src/routes/inventory.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const inventoryController = require('../controllers/inventory.controller');
 const { isAuthenticated } = require('../middlewares/security/authenticate.middleware');
+const { validateMongoId } = require('../validation/inputValidator')
 const router = express.Router();
 
-router.put('/:productId', isAuthenticated, inventoryController.updateInventory);
-router.get('/:productId', isAuthenticated, inventoryController.getInventory);
+router.put('/:productId', isAuthenticated, validateMongoId, inventoryController.updateInventory);
+router.get('/:productId', isAuthenticated, validateMongoId, inventoryController.getInventory);
 
 module.exports = router;
