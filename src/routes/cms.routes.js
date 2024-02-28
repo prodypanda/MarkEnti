7be19e7614071ef7644fb40287e14f725a5aa3c2
Prodypanda <admin@prodypanda.com>
2024-02-28 07:25:38 +0100

    Add input validation for MongoDB IDs in CMS routes.

--- src/routes/cms.routes.js
+++ src/routes/cms.routes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cmsController = require('../controllers/cms.controller');
 const { isAuthenticated } = require('../middlewares/security/authenticate.middleware');
+const { validateMongoId } = require('../validation/inputValidator')
 const router = express.Router();
 
+//adding validateMongoId to validate the mongo id
 router.post('/pages', isAuthenticated, cmsController.createPage);
-router.put('/pages/:id', isAuthenticated, cmsController.updatePage);
-router.delete('/pages/:id', isAuthenticated, cmsController.deletePage);
-router.get('/pages/:id', isAuthenticated, cmsController.getPage);
+router.put('/pages/:id', isAuthenticated, validateMongoId, cmsController.updatePage);
+router.delete('/pages/:id', isAuthenticated, validateMongoId, cmsController.deletePage);
+router.get('/pages/:id', isAuthenticated, validateMongoId, cmsController.getPage);
 router.get('/pages', isAuthenticated, cmsController.getPages);
 
 module.exports = router;
