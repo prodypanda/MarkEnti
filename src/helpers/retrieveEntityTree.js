7a58a2088f1e057604cb639fc05dbcc8be44f2b6
Prodypanda <admin@prodypanda.com>
2024-03-11 23:40:32 +0100

    📝 (retrieveEntityTree.js): Add comprehensive code comments to enhance readability and maintainability ♻️ (retrieveEntityTree.js): Refactor promise handling for better clarity and future maintenance

--- src/helpers/retrieveEntityTree.js
+++ src/helpers/retrieveEntityTree.js
@@ -1,11 +1,17 @@
 const Category = require('../models/category.model')
 const MenuItem = require('../models/menuItem.model')
 
-//entityType = category
-//parentId = null
-//sort = { name: 1 }
-
-//filter = active
+/**
+ * Retrieves a tree of entities by recursively fetching children of each entity based on entity type and parent ID.
+ *
+ * @param {string} entityType - The type of entity to retrieve (either 'category' or 'menuitem').
+ * @param {string} [parentId=null] - The ID of the parent entity to retrieve children for. Omit for top-level entities.
+ * @param {Object} filter - Filter conditions to apply to the query.
+ * @param {Object} sort - Sort criteria to apply to the query.
+ * @param {number} [startIndex=0] - Index of first entity to retrieve.
+ * @param {number} [limit=10] - Maximum number of entities to retrieve.
+ * @returns {Promise<Object[]>} Promise that resolves to the array of entity objects, with children included.
+ */
 const retrieveEntityTree = async (
   entityType,
   parentId = null,
@@ -29,7 +35,7 @@ const retrieveEntityTree = async (
     .sort(sort)
     .skip(startIndex)
     .limit(limit)
-  return await Promise.all(
+  const retret = await Promise.all(
     entities.map(async (entity) => {
       // Use the correct entity._id for the recursive call, and preserve the sort and filter options
       const children = await retrieveEntityTree(
@@ -44,8 +50,16 @@ const retrieveEntityTree = async (
       }
     })
   )
+  return retret
 }
 
+/**
+ * Returns the Mongoose model class for the given entityType.
+ *
+ * @param {string} entityType - The entity type ('category' or 'menuitem')
+ * @returns {Model} The Mongoose model class for the entity type
+ * @throws {Error} If entityType is invalid
+ */
 function getEntityModel(entityType) {
   switch (entityType) {
     case 'menuitem':
