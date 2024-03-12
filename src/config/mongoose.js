6c41d7fbcbf1f8118d790fc72275f9e2d945d9bd
Prodypanda <admin@prodypanda.com>
2024-03-12 01:11:52 +0100

    📝 (mongoose.js): update MongoDB connection function documentation for clarity ♻️ (mongoose.js): refactor database connection error handling to throw error instead of forcing process exit 💡 (mongoose.js): add comments to explain MongoDB connection setup and error handling changes

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -1,19 +1,27 @@
 const mongoose = require('mongoose')
 const connectionString = process.env.MONGODB_URI
 
+/**
+ * Connects to MongoDB database using provided URI.
+ * Handles connecting, setting strictQuery to false,
+ * and logging success/errors.
+ */
+/**
+ * Connects to a MongoDB database using Mongoose.
+ * @returns {Promise<void>} A promise that resolves when the connection is established or rejects if an error occurs.
+ */
 const connectDB = async () => {
   try {
     mongoose.set('strictQuery', false)
     const conn = await mongoose.connect(connectionString, {
       dbName: 'pandavee3db',
-
       ssl: false, // Ensure this is true for encrypted connections
     })
     console.log(`Database Connected: ${conn.connection.host}`)
   } catch (err) {
-    console.log('error')
-    console.error(err.message)
-    process.exit(1)
+    console.error('Error connecting to database:', err.message)
+    // process.exit(1)
+    throw err
   }
 }
 
